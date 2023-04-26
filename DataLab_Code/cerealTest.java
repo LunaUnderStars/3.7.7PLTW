4f98de1e861d7f1e77a38eece3479925982763b8
LunaUnderStars <Coolerthanu2005@gmail.com>
2023-04-26 18:02:36 +0000

    added functionality to read from file

--- DataLab_Code/cerealTest.java
+++ DataLab_Code/cerealTest.java
@@ -1,18 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+
+
 public class cerealTest {
 
     public static void main(String[] args) {
 
+        ArrayList<Cereal> cerealArrayList = new ArrayList<Cereal>();
+
+        /**
+        * Constructor for the Library class. Loads data from the childrensbooksearly1900s.txt file
+        * and populates the ArrayList childrensBooks.
+        */
+        {
+            // a try is like an if statement, "throwing" an error if the body of the try fails
+            try {
+                Scanner sc = new Scanner(new File("Cereal.csv"));
+                sc.nextLine();
+                while (sc.hasNextLine()) {
+                    // String method trim removes whitepsace from the beginning
+                    // and end of strings
+                    String temp = sc.nextLine().trim();
+                    // String method split splits this string based on the
+                    // specified token and returns an array of individual substrings
+                    String[] tokens = temp.split(",");
+                    String name = tokens[0];
+                    int sugar = Integer.parseInt(tokens[8]);
+                    double rating = Double.parseDouble(tokens[14]);
+
+                    // add the book to the array list
+                    cerealArrayList.add(new Cereal(name, sugar, rating));
+	            }
+            } catch (Exception e) { System.out.println("Error reading or parsing childrensbooksearly1900s.txt" + e); }
+        }
 
         Cereal HundredBran = new Cereal("100% Bran", 6, 68.402);
         System.out.println(HundredBran);
-
-
-
-
-
-
-
-
-    }
+        for (Cereal cereal: cerealArrayList) {
+            System.out.println(cereal);
+        }
     
+    }
 }
